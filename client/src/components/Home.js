a12236ebceb7c94039e15ccf142849fa35d28577
Adrian Darian <adarian@ucmerced.edu>
2019-01-05 18:28:54 -0800

    changed color of banner

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -13,7 +13,7 @@ class Home extends Component {
             <a id="mlh-trust-badge" style={{ display:"block", maxWidth:"100px", minWidth:"60px", position:"absolute", left:"50px", top:"0", width:"10%", zIndex:"10000"}}
               href="https://mlh.io/seasons/na-2019/events?utm_source=na-hackathon&amp;utm_medium=TrustBadge&amp;utm_campaign=2019-black&amp;utm_content=black"
               target="_blank">
-              <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-black.svg" alt="Major League Hacking 2019 Hackathon Season"
+              <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg" alt="Major League Hacking 2019 Hackathon Season"
               style={{width:"120%"}} className="mlh-badge" />
             </a>
           </div>
