a3c777be79b8c61074274214ea4dc4475322c0e6
Adrian Darian <adarian@ucmerced.edu>
2019-01-10 22:58:46 -0800

    please do not add .vscode to github

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -16,7 +16,7 @@ class Home extends Component {
           </div>
           <a id="mlh-trust-badge" style={{ display:"block", maxWidth:"100px", minWidth:"60px", position:"absolute", left:"50px", top:"0", width:"10%", zIndex:"10000"}}
             href="https://mlh.io/seasons/na-2019/events?utm_source=na-hackathon&amp;utm_medium=TrustBadge&amp;utm_campaign=2019-black&amp;utm_content=black"
-            target="_blank">
+            target="_blank" rel="noopener noreferrer">
             <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg" alt="Major League Hacking 2019 Hackathon Season"
             style={{width:"120%"}} className="mlh-badge" />
           </a>
