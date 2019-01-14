1293681d1cdc05d982553aad5459f6666bf940af
Adrian Darian <adarian@ucmerced.edu>
2019-01-14 15:53:56 -0800

    updated backend with proper mongodb connection queries

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const path = require('path');
+var bodyParser = require('body-parser');
+const morgan = require('morgan');
+
 const app = express();
+const port = process.env.port || 3000;
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+const application = path.join(__dirname, "/../client/build");
+app.use(express.static(application));
+
+
+// Starts the server and tells it to listen on port 3000
+app.listen(port, async () => {
+  console.log('Server listening on port ' + port);
 });
-
-app.listen(9000);
