6d794092fbbd6cb329fe4ec59f2ea42c9903ff03
Chaz <ctol1994@gmail.com>
2019-01-29 11:11:27 -0800

    Revert "fixed alignment issue in tracks"
    This reverts commit cda642a1f1d932f018e9a7f83e3b320d61344794.

--- client/src/components/Tracks.js
+++ client/src/components/Tracks.js
@@ -9,19 +9,17 @@ class Tracks extends Component {
       <ScrollAnimation animateIn="fadeIn" duration={1} animateOnce="true">
         <div className="container">
           <h2><u>TRACKS</u></h2>
-          <div className="themes">
-            <div className="theme">
-              <i class="fas fa-thumbs-up" style={{fontSize: 40, margin: "20px"}}></i>
-              <h3>Social Good</h3>
-            </div>
-            <div className="theme">
-              <i class="fas fa-user-tie" style={{fontSize: 40, margin: "20px"}}></i>
-              <h3>Entrepreneurship</h3>
-            </div>
-            <div className="theme">
-              <i class="fab fa-pagelines" style={{fontSize: 40, margin: "20px"}}></i>
-              <h3>Agritech</h3>
-            </div>
+          <div className="theme">
+            <i class="fas fa-thumbs-up" style={{fontSize: 40, margin: "20px"}}></i>
+            <h3>Social Good</h3>
+          </div>
+          <div className="theme">
+            <i class="fas fa-user-tie" style={{fontSize: 40, margin: "20px"}}></i>
+            <h3>Entrepreneurship</h3>
+          </div>
+          <div className="theme">
+            <i class="fab fa-pagelines" style={{fontSize: 40, margin: "20px"}}></i>
+            <h3>Agritech</h3>
           </div>
         </div>
         </ScrollAnimation>
