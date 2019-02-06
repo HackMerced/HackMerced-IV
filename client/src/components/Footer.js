cd29c7da9b5fcead436ec84c4e45390fe2203f67
Chaz <ctol1994@gmail.com>
2019-02-05 19:15:19 -0800

    added Merco and social to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,6 +6,11 @@ class Footer extends Component {
     return (
       <div className="Footer" id="Footer" >
         <div className="container">
+          <div className="social">
+            <a href="mailto:general@hackmerced.com?subject=HackMerced Questions&body=Dear HackMerced,"><i class="fas fa-envelope"></i></a>
+            <a href="https://www.facebook.com/hackmerced/" target="_blank"><i class="fab fa-facebook-square"></i></a>
+            <a href="https://www.instagram.com/hackmerced/" target="_blank"><i class="fab fa-instagram"></i></a>
+          </div>
           <div className="credits">
             <span className="copyright">
               © 2019 HackMerced. All Rights Reserved.
